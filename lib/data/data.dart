4ab2dc78efca1549611d38bc1847a3ecb177f3c8
sqxx <sqxx.kv@gmail.com>
2019-07-16 11:20:55 +1000

    [data] Добавлены направления форума

---
+++ lib/data/data.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'forum_direction.dart';
+
+class ProektoriaData {
+  static var forumDirections = <ForumDirection>[
+    ForumDirection("Технологии Материалов", "", Color(0xffcf73ca)),
+    ForumDirection("Технологии Движения", "", Color(0xfffa5558)),
+    ForumDirection("Космические Технологии", "", Color(0xff1d99d4)),
+    ForumDirection("Технологии Энергии", "", Color(0xff12bcf8)),
+    ForumDirection("IT Технологии", "", Color(0xfffdb94c)),
+    ForumDirection("Технологии Здоровья", "", Color(0xff6fcf8e)),
+  ];
+}
---
+++ lib/data/forum_direction.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ForumDirection {
+  String name;
+  String logoPath;
+  Color primaryColor;
+
+  ForumDirection(this.name, this.logoPath, this.primaryColor);
+}
