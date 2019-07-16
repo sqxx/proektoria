9f0d02a8251cc4636b5ae70ab7fac748688fd50b
sqxx <sqxx.kv@gmail.com>
2019-07-17 01:34:22 +1000

    Активирован экран выбора направления при запуске приложения

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:proektoria/ui/screens/main_screen.dart';
+import 'package:proektoria/ui/screens/direction_selector_screen.dart';
 
 void main() => runApp(App());
 
@@ -8,12 +8,12 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-        systemNavigationBarColor: Color(0xfff2f3f8),
+        systemNavigationBarColor: const Color(0xfff2f3f8),
         systemNavigationBarIconBrightness: Brightness.dark));
 
     return MaterialApp(
         title: 'ПроеКТОриЯ',
         debugShowCheckedModeBanner: false,
-        home: MainScreen() /*DirectionSelector()*/);
+        home: /*MainScreen()*/ DirectionSelectorScreen());
   }
 }
