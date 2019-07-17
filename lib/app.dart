60e2c7cd4d5c68dec85a01d5c3c3e9399822b065
sqxx <sqxx.kv@gmail.com>
2019-07-18 09:27:11 +1000

    [app] Выборочная навигация на основе сохранённых данных

--- lib/app.dart
+++ lib/app.dart
@@ -1,14 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:proektoria/data/colors.dart';
+import 'package:proektoria/data/profile.dart';
 import 'package:proektoria/ui/screens/direction_selector_screen.dart';
 import 'package:proektoria/ui/screens/main_screen.dart';
 import 'package:proektoria/ui/screens/student_direction_selector_screen.dart';
 
-import 'data/colors.dart';
-
 void main() => runApp(App());
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  bool _isProfileSaved;
+
+  void loadPreferences() async {
+    _isProfileSaved = await Profile.isProfileSaved();
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadPreferences();
+  }
+
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(
@@ -21,7 +39,7 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: 'ПроеКТОриЯ',
       debugShowCheckedModeBanner: false,
-      home: DirectionSelectorScreen(),
+      home: _isProfileSaved ? MainScreen() : DirectionSelectorScreen(),
       routes: {
         '/direction_selector': (context) => DirectionSelectorScreen(),
         '/student_direction_selector': (context) =>
