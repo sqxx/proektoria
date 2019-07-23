51a26a985238668bbcdab1083862571caa76cac7
sqxx <sqxx.kv@gmail.com>
2019-07-23 23:42:34 +1000

    [data/forum_data] Добавлено частичное расписание на 11 декабря

--- lib/data/forum_data.dart
+++ lib/data/forum_data.dart
@@ -43,5 +43,148 @@ class ForumData {
         end: DateTime(2019, 12, 10, 22, 30),
       ),
     ],
+    // 11 декабря
+    [
+      ForumEvent(
+        name: "Завтрак",
+        type: DirectionType.NONE,
+        venue: "Гостиницы",
+        start: DateTime(2019, 12, 11, 7, 00),
+        end: DateTime(2019, 12, 11, 8, 00),
+      ),
+      ForumEvent(
+        name: "Трансфер из гостиниц в «Арену»",
+        type: DirectionType.NONE,
+        venue: "---",
+        start: DateTime(2019, 12, 11, 8, 15),
+        end: DateTime(2019, 12, 11, 8, 45),
+      ),
+      ForumEvent(
+        name: "Знакомство по тематическим направлениям",
+        type: DirectionType.NONE,
+        venue: "Арена. Классы",
+        start: DateTime(2019, 12, 11, 9, 00),
+        end: DateTime(2019, 12, 11, 9, 30),
+      ),
+      ForumEvent(
+        name: "Торжественное открытие деловой образовательной программы",
+        type: DirectionType.NONE,
+        venue: "Арена. Главный зал",
+        start: DateTime(2019, 12, 11, 10, 00),
+        end: DateTime(2019, 12, 11, 10, 15),
+      ),
+      ForumEvent(
+        name:
+        "1 бинарный урок от победителя Всероссийского конкурса «Авторские уроки будущего» с отраслевым работодателем",
+        type: DirectionType.NONE,
+        venue: "Арена. Главный зал",
+        start: DateTime(2019, 12, 11, 10, 15),
+        end: DateTime(2019, 12, 11, 10, 30),
+      ),
+      ForumEvent(
+        name:
+        "2 бинарный урок от победителя Всероссийского конкурса «Авторские уроки будущего» с отраслевым работодателем",
+        type: DirectionType.NONE,
+        venue: "Арена. Главный зал",
+        start: DateTime(2019, 12, 11, 10, 30),
+        end: DateTime(2019, 12, 11, 10, 45),
+      ),
+      ForumEvent(
+        name:
+        "3 бинарный урок от победителя Всероссийского конкурса «Авторские уроки будущего» с отраслевым работодателем",
+        type: DirectionType.NONE,
+        venue: "Арена. Главный зал",
+        start: DateTime(2019, 12, 11, 10, 45),
+        end: DateTime(2019, 12, 11, 11, 00),
+      ),
+      ForumEvent(
+        name:
+        "4 бинарный урок от победителя Всероссийского конкурса «Авторские уроки будущего» с отраслевым работодателем",
+        type: DirectionType.NONE,
+        venue: "Арена. Главный зал",
+        start: DateTime(2019, 12, 11, 11, 00),
+        end: DateTime(2019, 12, 11, 11, 15),
+      ),
+      ForumEvent(
+        name:
+        "5 бинарный урок от победителя Всероссийского конкурса «Авторские уроки будущего» с отраслевым работодателем",
+        type: DirectionType.NONE,
+        venue: "Арена. Главный зал",
+        start: DateTime(2019, 12, 11, 11, 15),
+        end: DateTime(2019, 12, 11, 11, 30),
+      ),
+      ForumEvent(
+        name:
+        "6 бинарный урок от победителя Всероссийского конкурса «Авторские уроки будущего» с отраслевым работодателем",
+        type: DirectionType.NONE,
+        venue: "Арена. Главный зал",
+        start: DateTime(2019, 12, 11, 11, 30),
+        end: DateTime(2019, 12, 11, 11, 45),
+      ),
+      ForumEvent(
+        name: "Обед",
+        type: DirectionType.NONE,
+        venue: "Ресторан «Волга-Волга»",
+        start: DateTime(2019, 12, 11, 11, 45),
+        end: DateTime(2019, 12, 11, 13, 45),
+      ),
+      ForumEvent(
+        name: "Космические технологии",
+        type: DirectionType.SPACE,
+        venue: "Арена. Классы",
+        start: DateTime(2019, 12, 11, 13, 45),
+        end: DateTime(2019, 12, 11, 16, 30),
+      ),
+      ForumEvent(
+        name: "Технологии энергии",
+        type: DirectionType.ENERGY,
+        venue: "Арена. Классы",
+        start: DateTime(2019, 12, 11, 13, 45),
+        end: DateTime(2019, 12, 11, 16, 30),
+      ),
+      ForumEvent(
+        name: "Технологии движения",
+        type: DirectionType.MOVEMENT,
+        venue: "Арена. Классы",
+        start: DateTime(2019, 12, 11, 13, 45),
+        end: DateTime(2019, 12, 11, 16, 30),
+      ),
+      ForumEvent(
+        name: "Информационные технологии",
+        type: DirectionType.IT,
+        venue: "Арена. Классы",
+        start: DateTime(2019, 12, 11, 13, 45),
+        end: DateTime(2019, 12, 11, 16, 30),
+      ),
+      ForumEvent(
+        name: "Технологии материалов",
+        type: DirectionType.MATERIALS,
+        venue: "Арена. Классы",
+        start: DateTime(2019, 12, 11, 13, 45),
+        end: DateTime(2019, 12, 11, 16, 30),
+      ),
+      ForumEvent(
+        name: "Технологии здоровья",
+        type: DirectionType.HEALTH,
+        venue: "Арена. Классы",
+        start: DateTime(2019, 12, 11, 13, 45),
+        end: DateTime(2019, 12, 11, 16, 30),
+      ),
+    ]
   ];
+
+  static ForumDirection getForumDirectionByType(DirectionType type) {
+    if (type == DirectionType.NONE) return null;
+
+    ForumDirection fDirection;
+
+    directions.forEach((_) => {if (_.directionType == type) fDirection = _});
+
+    if (fDirection == null) {
+      throw Exception(
+          "Даный тип не представлен на форуме: " + type.index.toString());
+    } else {
+      return fDirection;
+    }
+  }
 }
