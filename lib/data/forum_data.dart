0d292b0df8aac0fc3ab4ec4e544ed57b97fad9b7
Valery Klachkov <sqxx.kv@gmail.com>
2019-11-07 15:26:04 +0300

    Исправлены даты в расписании форума

--- lib/data/forum_data.dart
+++ lib/data/forum_data.dart
@@ -1,21 +1,40 @@
-import 'package:proektoria/data/colors.dart';
 import 'package:proektoria/data/direction_type.dart';
 import 'package:proektoria/data/forum_direction.dart';
 import 'package:proektoria/data/forum_event.dart';
+import 'package:proektoria/ui/styles/AppColors.dart';
 
 class ForumData {
   static final directions = <ForumDirection>[
-    ForumDirection('Технологии Материалов', AppColors.MATERIALS_CLUSTER,
-        DirectionType.MATERIALS),
-    ForumDirection('Технологии Движения', AppColors.MOVEMENT_CLUSTER,
-        DirectionType.MOVEMENT),
     ForumDirection(
-        'Космические Технологии', AppColors.SPACE_CLUSTER, DirectionType.SPACE),
+      'Технологии Материалов',
+      AppColors.MATERIALS_CLUSTER,
+      DirectionType.MATERIALS,
+    ),
     ForumDirection(
-        'Технологии Энергии', AppColors.ENERGY_CLUSTER, DirectionType.ENERGY),
-    ForumDirection('IT Технологии', AppColors.IT_CLUSTER, DirectionType.IT),
+      'Технологии Движения',
+      AppColors.MOVEMENT_CLUSTER,
+      DirectionType.MOVEMENT,
+    ),
     ForumDirection(
-        'Технологии Здоровья', AppColors.HEALTH_CLUSTER, DirectionType.HEALTH),
+      'Космические Технологии',
+      AppColors.SPACE_CLUSTER,
+      DirectionType.SPACE,
+    ),
+    ForumDirection(
+      'Технологии Энергии',
+      AppColors.ENERGY_CLUSTER,
+      DirectionType.ENERGY,
+    ),
+    ForumDirection(
+      'IT Технологии',
+      AppColors.IT_CLUSTER,
+      DirectionType.IT,
+    ),
+    ForumDirection(
+      'Технологии Здоровья',
+      AppColors.HEALTH_CLUSTER,
+      DirectionType.HEALTH,
+    ),
   ];
 
   static final schedule = <List<ForumEvent>>[
@@ -25,22 +44,22 @@ class ForumData {
         name: "Заезд и размещение участников Форума. Регистрация",
         type: DirectionType.NONE,
         venue: "Гостиницы",
-        start: DateTime(2019, 12, 10, 11, 00),
-        end: DateTime(2019, 12, 10, 20, 00),
+        start: DateTime(2018, 12, 10, 11, 00),
+        end: DateTime(2018, 12, 10, 20, 00),
       ),
       ForumEvent(
         name: "Ужин",
         type: DirectionType.NONE,
         venue: "Гостиницы",
-        start: DateTime(2019, 12, 10, 19, 00),
-        end: DateTime(2019, 12, 10, 21, 00),
+        start: DateTime(2018, 12, 10, 19, 00),
+        end: DateTime(2018, 12, 10, 21, 00),
       ),
       ForumEvent(
         name: "Ознакомительная встреча школьников с кураторами",
         type: DirectionType.NONE,
         venue: "Гостиницы",
-        start: DateTime(2019, 12, 10, 21, 00),
-        end: DateTime(2019, 12, 10, 22, 30),
+        start: DateTime(2018, 12, 10, 21, 00),
+        end: DateTime(2018, 12, 10, 22, 30),
       ),
     ],
     // 11 декабря
@@ -49,125 +68,125 @@ class ForumData {
         name: "Завтрак",
         type: DirectionType.NONE,
         venue: "Гостиницы",
-        start: DateTime(2019, 12, 11, 7, 00),
-        end: DateTime(2019, 12, 11, 8, 00),
+        start: DateTime(2018, 12, 11, 7, 00),
+        end: DateTime(2018, 12, 11, 8, 00),
       ),
       ForumEvent(
         name: "Трансфер из гостиниц в «Арену»",
         type: DirectionType.NONE,
-        start: DateTime(2019, 12, 11, 8, 15),
-        end: DateTime(2019, 12, 11, 8, 45),
+        start: DateTime(2018, 12, 11, 8, 15),
+        end: DateTime(2018, 12, 11, 8, 45),
       ),
       ForumEvent(
         name: "Знакомство по тематическим направлениям",
         type: DirectionType.NONE,
         venue: "Арена. Классы",
-        start: DateTime(2019, 12, 11, 9, 00),
-        end: DateTime(2019, 12, 11, 9, 30),
+        start: DateTime(2018, 12, 11, 9, 00),
+        end: DateTime(2018, 12, 11, 9, 30),
       ),
       ForumEvent(
         name: "Торжественное открытие деловой образовательной программы",
         type: DirectionType.NONE,
         venue: "Арена. Главный зал",
-        start: DateTime(2019, 12, 11, 10, 00),
-        end: DateTime(2019, 12, 11, 10, 15),
+        start: DateTime(2018, 12, 11, 10, 00),
+        end: DateTime(2018, 12, 11, 10, 15),
       ),
       ForumEvent(
         name:
         "1 бинарный урок от победителя Всероссийского конкурса «Авторские уроки будущего» с отраслевым работодателем",
         type: DirectionType.NONE,
         venue: "Арена. Главный зал",
-        start: DateTime(2019, 12, 11, 10, 15),
-        end: DateTime(2019, 12, 11, 10, 30),
+        start: DateTime(2018, 12, 11, 10, 15),
+        end: DateTime(2018, 12, 11, 10, 30),
       ),
       ForumEvent(
         name:
         "2 бинарный урок от победителя Всероссийского конкурса «Авторские уроки будущего» с отраслевым работодателем",
         type: DirectionType.NONE,
         venue: "Арена. Главный зал",
-        start: DateTime(2019, 12, 11, 10, 30),
-        end: DateTime(2019, 12, 11, 10, 45),
+        start: DateTime(2018, 12, 11, 10, 30),
+        end: DateTime(2018, 12, 11, 10, 45),
       ),
       ForumEvent(
         name:
         "3 бинарный урок от победителя Всероссийского конкурса «Авторские уроки будущего» с отраслевым работодателем",
         type: DirectionType.NONE,
         venue: "Арена. Главный зал",
-        start: DateTime(2019, 12, 11, 10, 45),
-        end: DateTime(2019, 12, 11, 11, 00),
+        start: DateTime(2018, 12, 11, 10, 45),
+        end: DateTime(2018, 12, 11, 11, 00),
       ),
       ForumEvent(
         name:
         "4 бинарный урок от победителя Всероссийского конкурса «Авторские уроки будущего» с отраслевым работодателем",
         type: DirectionType.NONE,
         venue: "Арена. Главный зал",
-        start: DateTime(2019, 12, 11, 11, 00),
-        end: DateTime(2019, 12, 11, 11, 15),
+        start: DateTime(2018, 12, 11, 11, 00),
+        end: DateTime(2018, 12, 11, 11, 15),
       ),
       ForumEvent(
         name:
         "5 бинарный урок от победителя Всероссийского конкурса «Авторские уроки будущего» с отраслевым работодателем",
         type: DirectionType.NONE,
         venue: "Арена. Главный зал",
-        start: DateTime(2019, 12, 11, 11, 15),
-        end: DateTime(2019, 12, 11, 11, 30),
+        start: DateTime(2018, 12, 11, 11, 15),
+        end: DateTime(2018, 12, 11, 11, 30),
       ),
       ForumEvent(
         name:
         "6 бинарный урок от победителя Всероссийского конкурса «Авторские уроки будущего» с отраслевым работодателем",
         type: DirectionType.NONE,
         venue: "Арена. Главный зал",
-        start: DateTime(2019, 12, 11, 11, 30),
-        end: DateTime(2019, 12, 11, 11, 45),
+        start: DateTime(2018, 12, 11, 11, 30),
+        end: DateTime(2018, 12, 11, 11, 45),
       ),
       ForumEvent(
         name: "Обед",
         type: DirectionType.NONE,
         venue: "Ресторан «Волга-Волга»",
-        start: DateTime(2019, 12, 11, 11, 45),
-        end: DateTime(2019, 12, 11, 13, 45),
+        start: DateTime(2018, 12, 11, 11, 45),
+        end: DateTime(2018, 12, 11, 13, 45),
       ),
       ForumEvent(
         name: "Космические технологии",
         type: DirectionType.SPACE,
         venue: "Арена. Классы",
-        start: DateTime(2019, 12, 11, 13, 45),
-        end: DateTime(2019, 12, 11, 16, 30),
+        start: DateTime(2018, 12, 11, 13, 45),
+        end: DateTime(2018, 12, 11, 16, 30),
       ),
       ForumEvent(
         name: "Технологии энергии",
         type: DirectionType.ENERGY,
         venue: "Арена. Классы",
-        start: DateTime(2019, 12, 11, 13, 45),
-        end: DateTime(2019, 12, 11, 16, 30),
+        start: DateTime(2018, 12, 11, 13, 45),
+        end: DateTime(2018, 12, 11, 16, 30),
       ),
       ForumEvent(
         name: "Технологии движения",
         type: DirectionType.MOVEMENT,
         venue: "Арена. Классы",
-        start: DateTime(2019, 12, 11, 13, 45),
-        end: DateTime(2019, 12, 11, 16, 30),
+        start: DateTime(2018, 12, 11, 13, 45),
+        end: DateTime(2018, 12, 11, 16, 30),
       ),
       ForumEvent(
         name: "Информационные технологии",
         type: DirectionType.IT,
         venue: "Арена. Классы",
-        start: DateTime(2019, 12, 11, 13, 45),
-        end: DateTime(2019, 12, 11, 16, 30),
+        start: DateTime(2018, 12, 11, 13, 45),
+        end: DateTime(2018, 12, 11, 16, 30),
       ),
       ForumEvent(
         name: "Технологии материалов",
         type: DirectionType.MATERIALS,
         venue: "Арена. Классы",
-        start: DateTime(2019, 12, 11, 13, 45),
-        end: DateTime(2019, 12, 11, 16, 30),
+        start: DateTime(2018, 12, 11, 13, 45),
+        end: DateTime(2018, 12, 11, 16, 30),
       ),
       ForumEvent(
         name: "Технологии здоровья",
         type: DirectionType.HEALTH,
         venue: "Арена. Классы",
-        start: DateTime(2019, 12, 11, 13, 45),
-        end: DateTime(2019, 12, 11, 16, 30),
+        start: DateTime(2018, 12, 11, 13, 45),
+        end: DateTime(2018, 12, 11, 16, 30),
       ),
     ]
   ];
